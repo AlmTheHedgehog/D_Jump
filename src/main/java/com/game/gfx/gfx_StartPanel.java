c0e0cc6d54028ddbc11262c0eb89f36844f0a0ac
AlmTheHedgehog <tbe6666@gmail.com>
2022-07-20 14:35:17 +0200

    Butt added on start screen

--- src/main/java/com/game/gfx/gfx_StartPanel.java
+++ src/main/java/com/game/gfx/gfx_StartPanel.java
@@ -1,57 +1,82 @@
 package com.game.gfx;
 
+import java.util.HashMap;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.net.URL;
 
-public class gfx_StartPanel extends JPanel implements ActionListener, MouseListener{
+public class gfx_StartPanel extends JPanel{
     public static int P_HEIGHT, P_WIDTH;
+    private final int BUTT_WXH[] = {300, 150};
+    enum ButtStatus{PLAY_PRESSED, PLAY_UNPRESSED};
+    ButtStatus ButtStat;
+    ButtConteiner ButtCont;
+    GridBagConstraints PanelContainer;
     gfx_StartPanel(int height, int width){
         P_HEIGHT = height;
         P_WIDTH = width;
-        this.addMouseListener(this);
-        this.setPreferredSize(new Dimension(P_WIDTH, P_HEIGHT));
+        setLayout(new GridBagLayout());
+        PanelContainer = new GridBagConstraints();
+        ButtStat = ButtStatus.PLAY_UNPRESSED;
+        ButtCont = new ButtConteiner();
+        PanelContainer.insets = new Insets(P_HEIGHT/2,0,0,0);
+        add(ButtCont, PanelContainer);
+        setPreferredSize(new Dimension(P_WIDTH, P_HEIGHT));
     }
 
     public void paint(Graphics g){
         super.paint(g);
         Graphics2D g2D = (Graphics2D) g;
-        g2D.drawRect(12, 12, 100, 100);
+        g2D.drawImage(ButtCont.ButtImage, ButtCont.getX(), ButtCont.getY(), null);
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void mousePressed(MouseEvent e) {
-        System.out.println("x");
-        
-    }
-
-    @Override
-    public void mouseReleased(MouseEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void mouseEntered(MouseEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void mouseExited(MouseEvent e) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
+    public void makeRepaint(){
         repaint();
+    }
+
+    class ButtConteiner extends JPanel implements MouseListener{
+        private final HashMap<ButtStatus, URL> BUTT_IMG_PATH = new HashMap<ButtStatus, URL>(){{
+            put(ButtStatus.PLAY_PRESSED, getClass().getResource("/interface/Start_butt_pressed.png"));
+            put(ButtStatus.PLAY_UNPRESSED, getClass().getResource("/interface/Start_butt_unpressed.png"));
+        }};
+        Image ButtImage;
+        ButtConteiner(){
+            addMouseListener(this);
+            setPreferredSize(new Dimension(BUTT_WXH[0], BUTT_WXH[1]));
+            ButtImage = new ImageIcon(BUTT_IMG_PATH.get(ButtStat)).getImage();
+        }
+
+        void ChangeButtImage(){
+            ButtImage = new ImageIcon(BUTT_IMG_PATH.get(ButtStat)).getImage();
+            makeRepaint();
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            // TODO change screen
+            
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+            ButtStat = ButtStatus.PLAY_PRESSED;
+            ChangeButtImage();            
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            ButtStat = ButtStatus.PLAY_UNPRESSED;
+            ChangeButtImage();
+        }
+
+        @Override
+        public void mouseEntered(MouseEvent e) {           
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {            
+        }
     }
     
 }
--- src/main/java/com/game/gfx/gfx_frame.java
+++ src/main/java/com/game/gfx/gfx_frame.java
@@ -1,18 +1,19 @@
 package com.game.gfx;
 import javax.swing.*;
 import java.awt.event.*;
+import java.awt.*;
 
 
 public class gfx_frame extends JFrame implements KeyListener{
     public final int F_HEIGHT = 700, F_WIDTH = 470;
-    gfx_GamePanel plane;
+    gfx_GamePanel GamePlane;
     gfx_StartPanel StartPlane;
     public gfx_frame(){
-        plane = new gfx_GamePanel(F_HEIGHT, F_WIDTH);
+        GamePlane = new gfx_GamePanel(F_HEIGHT, F_WIDTH);
         StartPlane = new gfx_StartPanel(F_HEIGHT, F_WIDTH);
         this.setSize(F_WIDTH, F_HEIGHT);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.add(plane);
+        this.add(StartPlane);
         this.addKeyListener(this);
         this.pack();
         this.setResizable(false);
@@ -24,13 +25,13 @@ public class gfx_frame extends JFrame implements KeyListener{
     }
     @Override
     public void keyPressed(KeyEvent e) {
-        plane.main_hero.keysProcessing(KeyEvent.getKeyText(e.getKeyCode()).toLowerCase());
+        GamePlane.main_hero.keysProcessing(KeyEvent.getKeyText(e.getKeyCode()).toLowerCase());
     }
     @Override
     public void keyReleased(KeyEvent e) {
         if((KeyEvent.getKeyText(e.getKeyCode()) == "Right") ||
              (KeyEvent.getKeyText(e.getKeyCode()) == "Left")){
-            plane.main_hero.keysProcessing("release");
+                GamePlane.main_hero.keysProcessing("release");
         }
         
     }
